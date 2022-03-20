b597047275fd2ec9411ab05991c5e674482e3e07
yianan261 <ychen151@nyit.edu>
2022-03-20 09:39:15 -0700

    updated hardhat configuration

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+
+// // This is a sample Hardhat task. To learn how to create your own go to
+// // https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const projectId = "43996abd00574da789e54f0dc7e9aec1";
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [],
+    },
+    mainnet: { url: `https://polygon-mainnet.infura.io/v3/${porjectId}` },
+    accounts: [],
+  },
+  solidity: "0.8.4",
+};
