2e0d86dab78925f427340a466ab7346a298bcfa6
yianan261 <ychen151@nyit.edu>
2022-03-25 00:12:11 -0700

    reconfigured solhint.json and hardhat config
    make compiler version compatible with solidity

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,7 @@ require("@nomiclabs/hardhat-waffle");
 
 const fs = require("fs");
 //reference to private key
-const privateKey = fs.readFileSync(".secret").toString();
+const privateKey = fs.readFileSync("test/.secret").toString();
 const projectId = "43996abd00574da789e54f0dc7e9aec1";
 
 /**
@@ -27,26 +27,23 @@ module.exports = {
       chainId: 1337,
     },
     mumbai: {
+      //Infura
       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
       accounts: [privateKey],
     },
-    mainnet: { url: `https://polygon-mainnet.infura.io/v3/${porjectId}` },
-    accounts: [privateKey],
+    mainnet: {
+      //Infura
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
   },
   solidity: {
-    compilers: [
-      {
-        version: "0.5.8",
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
-      {
-        version: "0.8.4",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    ],
+    },
   },
 };
